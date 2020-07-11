f9f1bf764b86952b58bcd3f48b836a9c03017a12
jyrnan <yoeking@gmail.com>
2020-07-11 11:45:38 +0800

    Initial Commit

---
+++ FetchMeeBySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchMeeBySwiftUI
+//
+//  Created by jyrnan on 2020/7/11.
+//  Copyright © 2020 jyrnan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
