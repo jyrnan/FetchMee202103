bc67f6d3fb104dfde95bc7ac16802c48b4edc135
jyrnan <yoeking@gmail.com>
2020-11-02 22:54:06 +0800

    调整了UserInfo的视觉效果。

--- FetchMeeBySwiftUI/Views/ToolsView/UserInfo.swift
+++ FetchMeeBySwiftUI/Views/ToolsView/UserInfo.swift
@@ -27,21 +27,24 @@ struct UserInfo: View {
     @State var isNickNameInputShow: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ScrollView {
+
+        ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack{
+        
             ZStack {
-                GeometryReader {
-                    geometry in
+//                GeometryReader {
+//                    geometry in
                     Image(uiImage: fetchMee.users[userIDString!]?.banner ?? UIImage(named: "bg")!)
                         .resizable()
                         .aspectRatio(contentMode: .fill)
-                        .frame(width: geometry.size.width ,height: geometry.size.height - 55)
-                        .clipped()
-                        .padding(0)
-                }
+                        .overlay(LinearGradient.init(gradient: Gradient(colors: [Color(UIColor.systemBackground), Color.clear]), startPoint: .bottom, endPoint: .center))
+                        
+                        
+//                }
                 VStack {
                     Spacer()
                     HStack(alignment: .bottom) {
+                        Spacer()
                         ///个人信息大头像
                         Image(uiImage: fetchMee.users[userIDString!]?.avatar ?? UIImage(systemName: "person.circle")!)
                             .resizable()
@@ -61,103 +64,108 @@ struct UserInfo: View {
                             })
                         Spacer()
                         //
-                        NavigationLink(destination: ImageGrabView(userIDString: userIDString, userScreenName: userScreenName, timeline: userTimeline)){
-                        Image(systemName: "envelope")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(.accentColor)
-                            .padding(6)
-                            .frame(width: 32, height: 32, alignment: .center)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.accentColor, lineWidth: 1))}
-                        Image(systemName: (self.fetchMee.users[userIDString!]?.notifications == true ? "bell.fill" : "bell"))
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(fetchMee.users[userIDString!]?.notifications == true ? .white : .accentColor)
-                            .padding(6)
-                            .frame(width: 32, height: 32, alignment: .center)
-                            .background(fetchMee.users[userIDString!]?.notifications == true ? Color.accentColor : Color.clear)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.accentColor, lineWidth: 1))
-                        if fetchMee.users[userIDString!]?.isFollowing == true {
-                            Text("Following")
-                                .font(.body).bold()
-                                .frame(width: 84, height: 32, alignment: .center)
-                                .background(Color.accentColor)
-                                .foregroundColor(.white)
-                                .cornerRadius(16)
-                                .padding(.trailing, 16)
-                                .onTapGesture(count: 1, perform: {
-                                    fetchMee.unfollow(userIDString: userIDString!)
-                                })
-                        } else {
-                            Text("Follow")
-                                .font(.body).bold()
-                                .frame(width: 84, height: 32, alignment: .center)
-                                .background(Color.primary.opacity(0.1))
-                                .foregroundColor(.accentColor)
-                                .cornerRadius(16)
-                                .padding(.trailing, 16)
-                                .onTapGesture(count: 1, perform: {
-                                    fetchMee.follow(userIDString: userIDString!)
-                                })
-                        }
+//                        NavigationLink(destination: ImageGrabView(userIDString: userIDString, userScreenName: userScreenName, timeline: userTimeline)){
+//                        Image(systemName: "envelope")
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fit)
+//                            .foregroundColor(.accentColor)
+//                            .padding(6)
+//                            .frame(width: 32, height: 32, alignment: .center)
+//                            .clipShape(Circle())
+//                            .overlay(Circle().stroke(Color.accentColor, lineWidth: 1))}
+//                        Image(systemName: (self.fetchMee.users[userIDString!]?.notifications == true ? "bell.fill" : "bell"))
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fit)
+//                            .foregroundColor(fetchMee.users[userIDString!]?.notifications == true ? .white : .accentColor)
+//                            .padding(6)
+//                            .frame(width: 32, height: 32, alignment: .center)
+//                            .background(fetchMee.users[userIDString!]?.notifications == true ? Color.accentColor : Color.clear)
+//                            .clipShape(Circle())
+//                            .overlay(Circle().stroke(Color.accentColor, lineWidth: 1))
+//                        if fetchMee.users[userIDString!]?.isFollowing == true {
+//                            Text("Following")
+//                                .font(.body).bold()
+//                                .frame(width: 84, height: 32, alignment: .center)
+//                                .background(Color.accentColor)
+//                                .foregroundColor(.white)
+//                                .cornerRadius(16)
+//                                .padding(.trailing, 16)
+//                                .onTapGesture(count: 1, perform: {
+//                                    fetchMee.unfollow(userIDString: userIDString!)
+//                                })
+//                        } else {
+//                            Text("Follow")
+//                                .font(.body).bold()
+//                                .frame(width: 84, height: 32, alignment: .center)
+//                                .background(Color.primary.opacity(0.1))
+//                                .foregroundColor(.accentColor)
+//                                .cornerRadius(16)
+//                                .padding(.trailing, 16)
+//                                .onTapGesture(count: 1, perform: {
+//                                    fetchMee.follow(userIDString: userIDString!)
+//                                })
+//                        }
                         
                     }
-                }.padding(0)
-            }.frame(height:180)
+                    Spacer()
+                }.offset(x: 0, y: 90)
+            }
+            .frame(height:180)
             
-            ///用户信息View
             
-                VStack(alignment: .leading){
-                    HStack{
-                        VStack(alignment: .leading) {
+            //用户信息View
+            
+                VStack(alignment: .center){
+//                    HStack{
+//                        Spacer()
+                        VStack(alignment: .center) {
                             HStack {
-                                
-                                Text(fetchMee.users[userIDString!]?.name ?? "Name").font(.title2).bold()
+                                Spacer()
+                                Text(fetchMee.users[userIDString!]?.name ?? "Name").font(.body).bold()
                                 if !isNickNameInputShow {
-                                    Text(twitterUsers.filter{$0.userIDString == userIDString}.first?.nickName != nil ? "(\((twitterUsers.filter{$0.userIDString == userIDString}.first!).nickName!))" : "" ).font(.title2)
+                                    Text(twitterUsers.filter{$0.userIDString == userIDString}.first?.nickName != nil ? "(\((twitterUsers.filter{$0.userIDString == userIDString}.first!).nickName!))" : "" ).font(.body)
                                 }
-                                
-                                
+
                                 if isNickNameInputShow {
                                     HStack{
                                         //如果有nickname则在编辑窗内显示当前nickname，否则显示默认的字符"nickname"
-                                        
+
                                         TextField(twitterUsers.filter{$0.userIDString == userIDString}.first?.nickName != nil ? "(\((twitterUsers.filter{$0.userIDString == userIDString}.first!).nickName!))" : "nickname",
                                                   text: $nickNameText)
                                             .frame(width: 100)
                                             .textFieldStyle(RoundedBorderTextFieldStyle())
-                                        
+
                                         Button(action: {
-                                            
+
                                             saveOrUpdateTwitterUser()
                                             withAnimation{isNickNameInputShow = false}
                                         }){
-                                            Image(systemName: "arrow.forward.circle").foregroundColor(.accentColor).font(.title2)
+                                            Image(systemName: "arrow.forward.circle").foregroundColor(.accentColor).font(.body)
                                         }
-//                                        .disabled(nickNameText == "")
-                                        Spacer()
+//
+//                                        Spacer()
                                     }
                                 } else {
                                     Button(action: {
                                         withAnimation{isNickNameInputShow = true}
                                     }){
-                                        Image(systemName: "highlighter").foregroundColor(.gray).font(.title2)
+                                        Image(systemName: "highlighter").foregroundColor(.gray).font(.body)
                                     }
                                 }
+                                
+                                Spacer()
                             }
                             Text(fetchMee.users[userIDString!]?.screenName ?? "ScreenName")
-                                .font(.body).foregroundColor(.gray)
-                        }
-                        Spacer()
-                    }.padding(.top, 0)
-                    
+                                .font(.callout).foregroundColor(.gray)
+                        }.padding(.top, 60)
+//                        Spacer()
+//                    }.padding(.top, 60)
+
                     ///用户Bio信息
                     Text(fetchMee.users[userIDString!]?.description ?? "userBio").font(.body)
-                        .multilineTextAlignment(.leading)
+                        .multilineTextAlignment(.center)
                         .padding([.top], 16)
-                    
+
                     ///用户位置信息
                     HStack() {
                         Image(systemName: "location.circle").resizable().aspectRatio(contentMode: .fill).frame(width: 12, height: 12, alignment: .center).foregroundColor(.gray)
@@ -165,15 +173,16 @@ struct UserInfo: View {
                         Image(systemName: "calendar").resizable().aspectRatio(contentMode: .fill).frame(width: 12, height: 12, alignment: .center).foregroundColor(.gray).padding(.leading, 16)
                         Text(fetchMee.users[userIDString!]?.createdAt ?? "Unknow").font(.caption).foregroundColor(.gray)
                     }.padding(0)
-                    
+
                     ///用户following信息
                     HStack {
-                        Text(String(fetchMee.users[userIDString!]?.following ?? 0)).font(.body)
-                        Text("Following").font(.body).foregroundColor(.gray)
-                        Text(String(fetchMee.users[userIDString!]?.followed ?? 0)).font(.body).padding(.leading, 16)
-                        Text("Followers").font(.body).foregroundColor(.gray)
+                        Text(String(fetchMee.users[userIDString!]?.following ?? 0)).font(.callout)
+                        Text("Following").font(.callout).foregroundColor(.gray)
+                        Text(String(fetchMee.users[userIDString!]?.followed ?? 0)).font(.callout).padding(.leading, 16)
+                        Text("Followers").font(.callout).foregroundColor(.gray)
                     }.padding(.bottom, 16)
-                }.padding([.leading, .trailing], 16)
+                }
+                .padding([.leading, .trailing], 16)
                 
                 ///用户推文部分
                 ForEach(userTimeline.tweetIDStrings, id: \.self) {
@@ -181,7 +190,8 @@ struct UserInfo: View {
                     
                     Divider()
                     TweetRow(timeline: userTimeline, tweetIDString: tweetIDString)
-                }.onDelete(perform: { _ in print(#line, "delete")})
+                }
+                .onDelete(perform: { _ in print(#line, "delete")})
                 .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
                 
                 HStack {
@@ -192,9 +202,8 @@ struct UserInfo: View {
                         .foregroundColor(.gray)
                     Spacer()
                 } //下方载入更多按钮
-            }
-            
         }
+            }
 //        .ignoresSafeArea()
         .onAppear(){
             if self.firstTimeRun {
@@ -203,6 +212,7 @@ struct UserInfo: View {
                 userTimeline.refreshFromTop(for: userIDString)
             }
         }
+        .navigationTitle(Text(fetchMee.users[userIDString!]?.name ?? ""))
     }
 }
 
@@ -238,4 +248,10 @@ extension UserInfo {
             UINavigationBar.appearance().standardAppearance = barAppearance
             UINavigationBar.appearance().scrollEdgeAppearance = barAppearance
         }
+}
+
+struct UserInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        UserInfo()
+    }
 }
