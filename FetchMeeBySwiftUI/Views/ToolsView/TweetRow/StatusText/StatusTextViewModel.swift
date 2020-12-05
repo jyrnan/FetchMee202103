0cfdc57f7f2ac091b01a12be5c35d5f157892bd0
jyrnan <yoeking@gmail.com>
2020-12-05 12:10:21 +0800

    调整StatusText段落属性

--- FetchMeeBySwiftUI/Views/ToolsView/TweetRow/StatusText/StatusTextViewModel.swift
+++ FetchMeeBySwiftUI/Views/ToolsView/TweetRow/StatusText/StatusTextViewModel.swift
@@ -28,42 +28,77 @@ class StatusTextViewModel: ObservableObject {
             var attributedString = NSMutableAttributedString(string: string)
             let paragraphStyle = NSMutableParagraphStyle()
             paragraphStyle.lineSpacing = 4
+        paragraphStyle.paragraphSpacing = 8
             let range = NSMakeRange(0, (string as NSString).length)
             attributedString.addAttribute(.font, value: UIFont.preferredFont(forTextStyle: .body), range: range)
             attributedString.addAttribute(.foregroundColor, value: UIColor.label, range: range)
             attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: range)
             
-        guard let user_mentions = status["entities"]["user_mentions"].array, !user_mentions.isEmpty else {return attributedString}
+//        guard let user_mentions = status["entities"]["user_mentions"].array, !user_mentions.isEmpty else {return attributedString}
         
-        let mentionsAttribute = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body), .foregroundColor: UIColor.orange] as [NSAttributedString.Key : Any]
+//        let mentionsAttribute = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body), .foregroundColor: UIColor.orange] as [NSAttributedString.Key : Any]
+//
+//        for user_mention in user_mentions {
+//            if let begin: Int = user_mention["indices"].array?.first?.integer,
+//               let end: Int = user_mention["indices"].array?.last?.integer {
+//                let range: NSRange = NSRange(location: begin, length: end - begin)
+//                attributedString.addAttribute(.foregroundColor, value: UIColor.init(named: "TwitterBlue"), range: range)
+//            }
+//
+//        }
+            attributedString = setMentionAttribute(status: status, attributedString: attributedString)
         
-        for user_mention in user_mentions {
-            if let begin: Int = user_mention["indices"].array?.first?.integer,
-               let end: Int = user_mention["indices"].array?.last?.integer {
-                let range: NSRange = NSRange(location: begin, length: end - begin)
-                attributedString.addAttribute(.foregroundColor, value: UIColor.init(named: "TwitterBlue"), range: range)
-            }
             
-        }
         
             return attributedString
     }
     
     func setMentionAttribute(status: JSON, attributedString: NSMutableAttributedString) -> NSMutableAttributedString {
-        guard let user_mentions = status["entities"]["user_mentions"].array, !user_mentions.isEmpty else {return NSMutableAttributedString(string: "")}
+        guard let user_mentions = status["entities"]["user_mentions"].array, !user_mentions.isEmpty else {return attributedString}
         
-        let mentionsAttribute = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body), .foregroundColor: UIColor.orange] as [NSAttributedString.Key : Any]
+        let mentionsAttribute = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body), .foregroundColor: UIColor.init(named: "TwitterBlue")] as [NSAttributedString.Key : Any]
+        
+        ///设置一个换行初始位置
+        var wrapPosition = 0
         
         for user_mention in user_mentions {
             if let begin: Int = user_mention["indices"].array?.first?.integer,
                let end: Int = user_mention["indices"].array?.last?.integer {
                 let range: NSRange = NSRange(location: begin, length: end - begin)
                 attributedString.addAttributes(mentionsAttribute, range: range)
+                
+                if checkWrapPositionOfText(status: status, indice: end) {
+                    wrapPosition = end
+                }
             }
-            
         }
+        
+        attributedString.replaceCharacters(in: NSRange(location: wrapPosition, length: 1), with: "\n")
+        
+        let prefixedAttributedString = addReplyingToPrefix(attributedString: attributedString)
+        
+        return prefixedAttributedString
+    }
+    
+    func checkWrapPositionOfText(status: JSON, indice: Int) -> Bool{
+        
+        ///这里增加一个indice的判断，避免mention用户名出现在statusText的末位的情况
+        ///否则会出现读取indice后一个字符时候出现越界情况
+        guard let statusText = status["text"].string, indice != statusText.count else {return false}
+        
+        let index = statusText.index(statusText.startIndex, offsetBy: indice + 1)
+        return statusText[index] != "@"
+}
+    
+    func addReplyingToPrefix(attributedString: NSMutableAttributedString) -> NSMutableAttributedString {
+        
+        let replyingToprefix = NSMutableAttributedString(string: "Replying to ")
+        let range = NSMakeRange(0, replyingToprefix.string.count)
+        replyingToprefix.addAttribute(.foregroundColor, value: UIColor.gray, range: range)
+        replyingToprefix.addAttribute(.font, value: UIFont.preferredFont(forTextStyle: .body), range: range)
+        
+        attributedString.insert(replyingToprefix, at: 0)
         
         return attributedString
     }
-
 }
