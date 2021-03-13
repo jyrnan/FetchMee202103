59cf8694dee1ab89f9cd7fe6ffdaf9cef7d01b36
jyrnan <yoeking@gmail.com>
2021-03-13 21:22:38 +0800

    TwitterUser报错解决

--- FetchMeeBySwiftUI/Model/CoreData/Status_CD+JSON_Save.swift
+++ FetchMeeBySwiftUI/Model/CoreData/Status_CD+JSON_Save.swift
@@ -10,22 +10,22 @@ import Foundation
 import CoreData
 import Swifter
 
-//extension Status_CD {
-//    static func JSON_Save(from status: JSON) {
-//        let viewContext = PersistenceContainer.shared.container.viewContext
-//        let id = status["id_str"].string!
-//        var status: Status_CD
-//            
-//        let statusFetch: NSFetchRequest<Status_CD> = Status_CD.fetchRequest()
-//        statusFetch.predicate = NSPredicate(format: "%K = %@", #keyPath(Status_CD.idString), id)
-//            
-//            
-//            if let result = try? viewContext.fetch(statusFetch).first {
-//                status = result
-//            } else {
-//                status = Status_CD(context: viewContext)
-//            }
-//        
-//        
-//    }
-//}
+extension Status_CD {
+    static func JSON_Save(from status: JSON) {
+        let viewContext = PersistenceContainer.shared.container.viewContext
+        let id = status["id_str"].string!
+        var status: Status_CD
+            
+        let statusFetch: NSFetchRequest<Status_CD> = Status_CD.fetchRequest()
+        statusFetch.predicate = NSPredicate(format: "%K = %@", #keyPath(Status_CD.id_str), id)
+            
+            
+            if let result = try? viewContext.fetch(statusFetch).first {
+                status = result
+            } else {
+                status = Status_CD(context: viewContext)
+            }
+        
+        
+    }
+}
